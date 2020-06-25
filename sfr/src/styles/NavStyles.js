300f59737107ffd75b32df21d0a04c3eec5ff371
Virginia Davenport <virginialdport@gmail.com>
2020-06-25 11:09:13 -0600

    started to add styles

---
+++ sfr/src/styles/NavStyles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+const NavStyles = styled.header`
+    background-color:#CB5B04;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 3px;
+
+    h1{
+        font-family: 'Great Vives', cursive; 
+        font-size: 3rem;
+        margin: 0;
+        padding: 0 3px;
+
+    }
+
+    button{
+        background: none;
+        margin: 0 10px 0 0;
+    }
+
+    .link{
+        text-decoration: none;
+        border: none;
+        font-family: 'Montserrat', sans-serif;
+        font-size: 1.2rem;
+        color: #220C00;
+        background-color: #BCF274;
+        padding: 2px 4px;
+        border-radius: 10px;
+    }
+
+`;
+export default NavStyles
---
+++ sfr/src/styles/RegisterStyles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+const RegisterStyles = styled.div`
+    display:flex;
+    justify-content: center;
+
+    .register-container{
+        background-color: #CB5B04;
+        margin: 50px 0;
+        width: 500px;
+        padding: 5%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        line-height: 2rem;
+        border-radius: 10px;
+        color: #220C00;        
+    }
+
+    button{
+        text-decoration: none;
+        border: none;
+        font-family: 'Montserrat', sans-serif;
+        font-size: 1.2rem;
+        color: #220C00;
+        background-color: #BCF274;
+        padding: 2px 4px;
+        border-radius: 10px;
+       
+    }
+
+`;
+export default RegisterStyles
